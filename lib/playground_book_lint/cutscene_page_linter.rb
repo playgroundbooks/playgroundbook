81e238de60dbc4de8e4d9c7a2e74159321fb791f
rpowelll <rhys@rpowell.me>
2016-07-26 21:53:18 +1000

    Add cutscene page linter

---
+++ lib/playground_book_lint/cutscene_page_linter.rb
@@ -0,0 +1,10 @@
+require 'playground_book_lint/abstract_linter'
+
+module PlaygroundBookLint
+  # A linter for verifying cutscene pages
+  class CutscenePageLinter < AbstractLinter
+    def lint
+      # TODO: Lint the cutscene page manifest here
+    end
+  end
+end
