a7f90a532eab057c52db2c9c9cc8720582a68e34
rpowelll <rhys@rpowell.me>
2016-07-26 22:30:23 +1000

    Add cutscene page manifest linter

--- lib/playground_book_lint/cutscene_page_linter.rb
+++ lib/playground_book_lint/cutscene_page_linter.rb
@@ -1,10 +1,17 @@
 require 'playground_book_lint/abstract_linter'
+require 'playground_book_lint/cutscene_page_manifest_linter'
 
 module PlaygroundBookLint
   # A linter for verifying cutscene pages
   class CutscenePageLinter < AbstractLinter
+    attr_accessor :cutscene_page_manifest_linter
+
+    def initialize(cutscene_page_manifest_linter = CutscenePageManifestLinter.new)
+      @cutscene_page_manifest_linter = cutscene_page_manifest_linter
+    end
+
     def lint
-      # TODO: Lint the cutscene page manifest here
+      cutscene_page_manifest_linter.lint
     end
   end
 end
