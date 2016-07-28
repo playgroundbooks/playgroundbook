5cbd978d7a5056d1d3305d476f0a9423b7a2cb53
rpowelll <rhys@rpowell.me>
2016-07-28 22:07:03 +1000

    Require the presence of a cutscene file

--- lib/playground_book_lint/cutscene_page_manifest_linter.rb
+++ lib/playground_book_lint/cutscene_page_manifest_linter.rb
@@ -8,10 +8,9 @@ module PlaygroundBookLint
     def lint
       # Cutscene references should point to an existent HTML file
       cutscene_reference = manifest_plist_contents['CutsceneReference']
-      unless cutscene_reference.nil?
-        fail_lint "Cutscene file at '#{cutscene_reference}' must be HTML" unless cutscene_reference =~ /^.+\.html$/i
-        fail_lint "Cutscene file at '#{cutscene_reference}' doesn't exist" unless File.exist? cutscene_reference
-      end
+      fail_lint "Cutscene manifest doesn't reference a cutscene file" if cutscene_reference.nil?
+      fail_lint "Cutscene file at '#{cutscene_reference}' isn't HTML" unless cutscene_reference =~ /^.+\.html$/i
+      fail_lint "Cutscene file at '#{cutscene_reference}' doesn't exist" unless File.exist? cutscene_reference
     end
   end
 end
