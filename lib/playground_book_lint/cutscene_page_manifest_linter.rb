5be72ff04904370f3da5741144f01e72b745570b
Rhys Powell <rhys@rpowell.me>
2016-07-28 19:18:01 +1000

    Check that cutscene references are valid

--- lib/playground_book_lint/cutscene_page_manifest_linter.rb
+++ lib/playground_book_lint/cutscene_page_manifest_linter.rb
@@ -1,6 +1,17 @@
 require 'playground_book_lint/manifest_linter'
 
 module PlaygroundBookLint
+  # A linter for verifying the contents of a cutscene page's manifest
   class CutscenePageManifestLinter < ManifestLinter
+    attr_accessor :page_manifest_linter
+
+    def lint
+      # Cutscene references should point to an existant HTML file
+      cutscene_reference = manifest_plist_contents['CutsceneReference']
+      unless cutscene_reference.nil?
+        fail_lint "Cutscene file at '#{cutscene_reference}' must be HTML" unless cutscene_reference =~ /.*.\.html$/i
+        fail_lint "Cutscene file at '#{cutscene_reference}' doesn't exist" unless File.exist? cutscene_reference
+      end
+    end
   end
 end
--- spec/playground_book_lint/cutscene_page_manifest_linter_spec.rb
+++ spec/playground_book_lint/cutscene_page_manifest_linter_spec.rb
@@ -12,5 +12,48 @@ module PlaygroundBookLint
         expect { cutscene_page_manifest_linter.lint }.to_not raise_error
       end
     end
+
+    context 'given a cutscene path' do
+      it "fails if that file doesn't exist" do
+        FakeFS do
+          cutscene_reference = 'FakeFile.html'
+          plist = {
+            'Name' => 'Test Page',
+            'CutsceneReference' => cutscene_reference
+          }.to_plist
+          File.open('Manifest.plist', 'w') { |f| f.write(plist) }
+
+          expect { cutscene_page_manifest_linter.lint }.to raise_error "Cutscene file at 'FakeFile.html' doesn't exist"
+        end
+      end
+
+      it 'fails with a non-HTML file' do
+        FakeFS do
+          cutscene_reference = 'RealFile.xml'
+          plist = {
+            'Name' => 'Test Page',
+            'CutsceneReference' => cutscene_reference
+          }.to_plist
+          File.open('Manifest.plist', 'w') { |f| f.write(plist) }
+          File.open(cutscene_reference, 'w')
+
+          expect { cutscene_page_manifest_linter.lint }.to raise_error "Cutscene file at '#{cutscene_reference}' must be HTML"
+        end
+      end
+
+      it 'passes with a valid file' do
+        FakeFS do
+          cutscene_reference = 'RealFile.html'
+          plist = {
+            'Name' => 'Test Page',
+            'CutsceneReference' => cutscene_reference
+          }.to_plist
+          File.open('Manifest.plist', 'w') { |f| f.write(plist) }
+          File.open(cutscene_reference, 'w')
+
+          expect { cutscene_page_manifest_linter.lint }.not_to raise_error
+        end
+      end
+    end
   end
 end
